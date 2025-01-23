a566131db51672da32ad105f810f2b0fcf6db933
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-23 16:48:47 +0800

    Define routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    { path: '/', element: <HomePage /> },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;

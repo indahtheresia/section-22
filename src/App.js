d32151320945eda048d6a335cb862f95506f1a63
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-23 16:55:58 +0800

    Add a second route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <HomePage /> },
+    { path: '/products', element: <ProductPage />},
   ]);
 
   return <RouterProvider router={router} />;

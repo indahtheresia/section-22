68d385fa3b2e772159a1f74e4b925e9f7e763dc1
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-24 10:13:10 +0800

    Define & use dynamic routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 function App() {
   // const routeDefinitions = createRoutesFromElements(
@@ -22,6 +23,7 @@ function App() {
       children: [
         { path: '/', element: <HomePage /> },
         { path: '/products', element: <ProductPage />},
+        { path: '/products/:productId', element: <ProductDetailPage />},
       ],
     },
   ]);
